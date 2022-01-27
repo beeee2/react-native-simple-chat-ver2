ff17372b1e28a9d2e3e334e7d63af04055fa2ee0
beeee2 <beeee2@naver.com>
2022-01-27 15:58:31 +0900

    create channellist style

--- src/screens/ChannelList.js
+++ src/screens/ChannelList.js
@@ -1,10 +1,36 @@
-import React from 'react';
-import styled from 'styled-components/native';
+import React, { useContext } from 'react';
+import { FlatList } from 'react-native';
+import styled, { ThemeContext } from 'styled-components/native';
 import { Text, Button } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
 
 const Container = styled.View`
     flex : 1;
     background-color : ${({theme}) => theme.background };
+`;
+const ItemContainer = styled.TouchableOpacity`
+    flex-direction : row;
+    align-items : center;
+    border-bottom-width: 1px;
+    border-color : ${({ theme }) => theme.listBorder};
+    padding: 15px 20px;
+`;
+const ItemTextContainer = styled.View`
+    flex: 1;
+    flex-direction: column;
+`;
+const ItemTitle = styled.Text`
+    font-size: 20px;
+    font-weight: 600;
+`;
+const ItemDescription = styled.Text`
+    font-size: 16px;
+    margin-top: 5px;
+    color : ${({theme}) => theme.listTime};
+`;
+const ItemTime = styled.Text`
+    font-size: 12px;
+    color: ${({theme}) => theme.listTime};
 `;
 
 const ChannelList = ({navigation}) => {
