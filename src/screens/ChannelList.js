8a3830863a002e5c090983203c8cd015daac33c9
beeee2 <beeee2@naver.com>
2022-01-30 23:27:18 +0900

    Modify ChannelList.js

--- src/screens/ChannelList.js
+++ src/screens/ChannelList.js
@@ -29,7 +29,7 @@ const ItemDescription = styled.Text`
     color : ${({theme}) => theme.listTime};
 `;
 const ItemTime = styled.Text`
-    font-size: 13px;
+    font-size: 14px;
     color: ${({theme}) => theme.listTime};
 `;
 
